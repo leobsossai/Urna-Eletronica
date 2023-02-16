74392cc5428271613ff8ac52b4e96848ee4ffa17
Leonardo Bueno Sossai <leosossai.ls@gmail.com>
2023-02-16 11:47:32 -0300

    urna finalizada

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// declaração de variávies de controle de interface
 const telaPrincipal = document.querySelector('.d-1');
 const telaInferior = document.querySelector('.d-2');
 const seuVotoPara = document.querySelector('.d-1-1 span');
@@ -6,16 +7,23 @@ const telaNumeros = document.querySelector('.d-1-3');
 const telaDescricao = document.querySelector('.d-1-4');
 const telaFotos = document.querySelector('.d-1-right');
 
+//Varáveis de controle de ambiente:
 let etapaAtual = 0;
 let numero = '';
+let votoBranco = false;
+let votoNulo = false;
+let votosConfirmados = [];
+let etapa = etapas[etapaAtual];
+let finalizado = false;
+let nomeCandidato = '';
 
+// carrega a etapa da votação se baseando na etapa atual
 function carregarEtapa() {
-    let etapa = etapas[etapaAtual];
-
-    seuVotoPara.innerHTML = '';
+    seuVotoPara.style.display = 'none';
+    telaNumeros.style.display = 'flex';
     telaTitulo.innerHTML = etapa.titulo;
     telaDescricao.innerHTML = '';
-    telaFotos.style.display = 'none';
+    telaFotos.innerHTML = '';
     telaInferior.style.display = 'none';
 
     for(let i = 0; i < etapa.numeros; i++) {
@@ -26,11 +34,38 @@ function carregarEtapa() {
         };
     };
 };
-
+// mostra na tela as informações referentes ao numero digitado
 function atualizaInterface() {
-    alert('votou');
-}
-
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        telaInferior.style.display = 'block';
+        telaDescricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}<br/>`;
+        nomeCandidato = candidato.nome;
+        fotosHTML = '';
+        for (let i in candidato.fotos) {
+            if (candidato.fotos[i].small) {
+                fotosHTML += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}">${candidato.fotos[i].legenda}</div>`
+            } else {
+                fotosHTML += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}">${candidato.fotos[i].legenda}</div>`
+            };
+        };
+        telaFotos.innerHTML = fotosHTML;
+    } else { //voto nulo se os numeros digitados não corresponderem a nenhum candidato ou nenhum numero seja digitado
+        seuVotoPara.style.display = 'block';
+        telaInferior.style.display = 'block';
+        telaDescricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>'
+        votoNulo = true;
+    };
+};
+// adiciona os numeros a tela
 function clicou(n) {
     let telaNumero = document.querySelector('.d-1-3 .numero.pisca');
     if (telaNumero !== null) {
@@ -45,14 +80,53 @@ function clicou(n) {
         };
     };
 };
+// define voto em branco ao clicar no botao
 function branco() {
-    alert('branco');
+    votoBranco = true;
+    numero = '';
+    telaNumeros.innerHTML = '';
+    seuVotoPara.style.display = 'block';
+    telaInferior.style.display = 'block';
+    telaDescricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>'
 };
+// reseta a tela e a votação da etapa atual
 function corrige() {
-    alert('corrige');
+    if (finalizado == false) {
+        carregarEtapa();
+        numero = '';
+    };
 };
+// confirma o voto, seja ele em branco, nulo ou para um dos candidatos
 function confirma() {
-    alert('confirma');
+    if (etapas[etapaAtual + 1] !== undefined) {
+        if (votoBranco && votosConfirmados.length < etapas.length) {
+            votosConfirmados.push(`${etapa.titulo}: voto em branco`);
+        } else if (votoNulo && votosConfirmados.length < etapas.length) {
+            votosConfirmados.push(`${etapa.titulo}: voto nulo`);
+        } else if (votosConfirmados.length < etapas.length) {
+            votosConfirmados.push(`${etapa.titulo}: ${nomeCandidato}`);
+        };
+        numero = '';
+        etapaAtual++;
+        etapa = etapas[etapaAtual];
+        carregarEtapa();
+    } else {
+        numero = '';
+        seuVotoPara.style.display = 'none';
+        telaNumeros.innerHTML = '';
+        telaTitulo.innerHTML = '';
+        telaDescricao.innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
+        telaFotos.style.display = 'none';
+        telaInferior.style.display = 'none';
+        if (votoBranco && votosConfirmados.length < etapas.length) {
+            votosConfirmados.push(`${etapa.titulo}: voto em branco`);
+        } else if (votoNulo && votosConfirmados.length < etapas.length) {
+            votosConfirmados.push(`${etapa.titulo}: voto nulo`);
+        } else if (votosConfirmados.length < etapas.length) {
+            votosConfirmados.push(`${etapa.titulo}: ${nomeCandidato}`);
+        };
+        finalizado = true;
+    };
 };
-
+// inicializa a urna!
 carregarEtapa();
